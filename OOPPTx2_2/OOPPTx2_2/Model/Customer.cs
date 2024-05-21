e03fe827463e360f68a6577dff71618e95d1b70c
Quan2412 <156040821+Quan2412@users.noreply.github.com>
2024-05-22 00:06:49 +0700

    Add files via upload

---
+++ OOPPTx2_2/OOPPTx2_2/Model/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPTx2_2.Model
+{
+    // Customer class inherit from Person class
+    public class Customer : Person
+    {
+        public double Balance { get; set; }
+
+        public override void Display()
+        {
+            Console.WriteLine($"Customer - Name: {Name}, Age: {Age}, Address: {Address}, Balance: {Balance}");
+        }
+    }
+}
---
+++ OOPPTx2_2/OOPPTx2_2/Model/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPTx2_2.Model
+{
+    // Abstract class Person for inheritance
+    public abstract class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+
+        public abstract void Display();
+    }
+}
