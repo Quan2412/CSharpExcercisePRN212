7fa583a8ab2722c0114d69b88cc65cbf07b10e01
Quan2412 <156040821+Quan2412@users.noreply.github.com>
2024-05-21 23:36:35 +0700

    Add files via upload

---
+++ OOPTx2_1/OOPTx2_1/Model/PartTimeEmployee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPTx2_1.Model
+{
+    public class PartTimeEmployee : Employee
+    {
+        public double WorkingHours { get; set; }
+
+        public override double CalculateSalary()
+        {
+            return WorkingHours * PaymentPerHour;
+        }
+
+        public override string ToString()
+        {
+            return $"Part-time Employee - {base.ToString()}, Working Hours: {WorkingHours}, Salary: {CalculateSalary()}";
+        }
+    }
+}
